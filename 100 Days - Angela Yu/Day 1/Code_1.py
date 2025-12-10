9848c56a835c8fdb6d4cc1fddcfdd6addeca23eb
roro-a1 <rokaderohan.40@gmail.com>
2025-12-10 21:43:29 +0530

    Day 1

---
+++ 100 Days - Angela Yu/Day 1/Code_1.py
@@ -0,0 +1,44 @@
+# Write your code below this code.
+"""
+print("Hello world!")
+print("Hello world!")
+"""
+
+# For printing on next line.
+"""
+print("Hello world!\nHello world!") 
+"""
+
+# String concatenation
+"""
+print("Hello" + "Angela!") 
+print("Hello " + "Angela!")
+print("Hello" + " Angela!")
+print("Hello" + " " + "Angela!")
+"""
+
+# Taking input from the user.
+"""
+print("Hello " + input("What is your name?"))
+"""
+"""
+print("Hello " + input("What is your name?") + "!")
+"""
+
+# Python Variable. - Used for storing values.
+"""
+name = input("What is your name?")
+print(name)
+"""
+"""
+name = input("What is your name?")
+print(len(name))
+"""
+"""
+print(len(input("What is your name?")))
+"""
+"""
+user_name = input("What is your name?")
+length = len(username)
+print(length)
+"""
---
+++ 100 Days - Angela Yu/Day 1/Project.py
@@ -0,0 +1,6 @@
+# Band Name Generator project.
+
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be: " + city + " " + pet_name)
